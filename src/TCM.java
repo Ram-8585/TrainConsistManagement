035b4e7ab771c56014f3d39e6dac21fc82171d69
Ram-8585 <janakiram8585@gmail.com>
2026-04-09 09:13:35 +0530

    UC1: Display Consist Summary

--- src/TCM.java
+++ src/TCM.java
@@ -1,2 +1,9 @@
+import java.util.*;
+
 public class TCM {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Initial bogie count: " + trainConsist.size());
+    }
 }
